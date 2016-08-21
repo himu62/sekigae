07dde8a9b77491946a2043b89d8c78a5bd7e5f35
himu62 <zero.256dic@gmail.com>
2016-08-22 02:44:01 +0900

    add user model functions

--- model/user.go
+++ model/user.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"database/sql"
+)
+
 type (
 	// User is profile of a Treasure student
 	User struct {
@@ -9,3 +13,78 @@ type (
 		Image  string
 	}
 )
+
+func FindUsers(db *sql.DB) ([]User, error) {
+	users := make([]User, 0, 30)
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.ID, &u.Name, &u.Github, &u.Image); err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (user *User) Insert(db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	if _, err := tx.Query(
+		"INSERT INTO users(name, github, image) VALUES(?, ?, ?)",
+		user.Name,
+		user.Github,
+		user.Image,
+	); err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+func (user *User) Update(db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	if _, err := tx.Query(
+		"UPDATE users SET name = ?, github = ?, image = ?",
+		user.Name,
+		user.Github,
+		user.Image,
+	); err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+func (user *User) Delete(db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	if _, err := tx.Query(
+		"DELETE FROM users WHERE id = ?",
+		user.ID,
+	); err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}

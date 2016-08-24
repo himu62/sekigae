662cda9f89e0f8ee7b7ff95ed0e87acde6ad7e82
himu62 <zero.256dic@gmail.com>
2016-08-24 10:48:21 +0900

    seat controller

--- controller/seat.go
+++ controller/seat.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
+	"github.com/himu62/sekigae/db"
 	"github.com/himu62/sekigae/model"
 )
 
@@ -16,23 +19,44 @@ func AddSeatRoutes(r *gin.RouterGroup) {
 
 	g := r.Group("/seats")
 	{
-		g.GET("/", ctl.fetch)
+		g.GET("/:id", ctl.fetch)
 		g.GET("/list", ctl.list)
-		g.POST("/", ctl.create)
+		g.POST("", ctl.create)
 	}
 }
 
 func (ctl *Seat) fetch(c *gin.Context) {
-	data := &model.Seats{}
-
-	// jst := time.FixedZone("Asia/Tokyo", 9*60*60)
-	// data.Created.In(jst)
+	id, err := strconv.ParseUint(c.Param("id"), 10, 0)
+	if err != nil {
+		c.JSON(400, NewError("request parameter is wrong."))
+		return
+	}
+	db, err := db.New()
+	if err != nil {
+		c.JSON(500, NewError("failed to connect DB"))
+		return
+	}
+	data, err := model.FindSeatsByID(db, uint32(id))
+	if err != nil {
+		c.JSON(500, NewError("failed to fetch seats"))
+		return
+	}
 
 	c.JSON(200, data)
 }
 
 func (ctl *Seat) list(c *gin.Context) {
-	data := make([]model.Seats, 5)
+	db, err := db.New()
+	if err != nil {
+		c.JSON(500, NewError("failed to connect DB"))
+		return
+	}
+	data, err := model.FindSeats(db)
+	if err != nil {
+		c.JSON(500, NewError("failed to fetch seats"))
+		return
+	}
+
 	c.JSON(200, data)
 }
 

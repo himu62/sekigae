429dc45a37b4c946071494ade50e205531d54f12
himu62 <zero.256dic@gmail.com>
2016-08-24 17:24:34 +0900

    update controllers

--- controller/seat.go
+++ controller/seat.go
@@ -9,17 +9,17 @@ import (
 )
 
 type (
-	// Seat controller
+	// Seats controller
 	Seat struct{}
 )
 
-// AddSeatRoutes make routes of seat controller
+// AddSeatsRoutes make routes of seats controller
 func AddSeatRoutes(r *gin.RouterGroup) {
 	ctl := &Seat{}
 
 	g := r.Group("/seats")
 	{
-		g.GET("/:id", ctl.fetch)
+		g.GET("/detail/:id", ctl.fetch)
 		g.GET("/list", ctl.list)
 		g.POST("", ctl.create)
 	}
@@ -28,39 +28,55 @@ func AddSeatRoutes(r *gin.RouterGroup) {
 func (ctl *Seat) fetch(c *gin.Context) {
 	id, err := strconv.ParseUint(c.Param("id"), 10, 0)
 	if err != nil {
-		c.JSON(400, NewError("request parameter is wrong."))
+		error(c, 500, "equest parameter is wrong.")
 		return
 	}
 	db, err := db.New()
 	if err != nil {
-		c.JSON(500, NewError("failed to connect DB"))
+		error(c, 500, "failed to fetch DB.")
 		return
 	}
-	data, err := model.FindSeatsByID(db, uint32(id))
+	seat, err := model.FindSeatByID(db, uint32(id))
 	if err != nil {
-		c.JSON(500, NewError("failed to fetch seats"))
+		error(c, 500, "failed to fetch seat.")
 		return
 	}
 
-	c.JSON(200, data)
+	c.JSON(200, seat)
 }
 
 func (ctl *Seat) list(c *gin.Context) {
 	db, err := db.New()
 	if err != nil {
-		c.JSON(500, NewError("failed to connect DB"))
+		error(c, 500, "failed to connect DB.")
 		return
 	}
-	data, err := model.FindSeats(db)
+	seats, err := model.FindSeats(db)
 	if err != nil {
-		c.JSON(500, NewError("failed to fetch seats"))
+		error(c, 500, "failed to fetch seats.")
 		return
 	}
 
-	c.JSON(200, data)
+	c.JSON(200, seats)
 }
 
 func (ctl *Seat) create(c *gin.Context) {
-	data := &model.Seats{}
-	c.JSON(200, data)
+	seat, err := model.NewSeat(c)
+	if err != nil {
+		error(c, 400, "request parameter is wrong.")
+		return
+	}
+
+	db, err := db.New()
+	if err != nil {
+		error(c, 500, "failed to connect DB.")
+		return
+	}
+
+	if err := seat.Insert(db); err != nil {
+		error(c, 500, "failed to insert record.")
+		return
+	}
+
+	c.JSON(201, seat)
 }
